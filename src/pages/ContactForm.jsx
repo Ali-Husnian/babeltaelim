36b9ffbc8c021c7f35963bd31daa8d2d1fb47c33
ali husnain <pkihusnain220@gmail.com>
2024-08-07 01:12:08 +0500

    send email

--- src/pages/ContactForm.jsx
+++ src/pages/ContactForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +12,7 @@ const ContactForm = () => {
   });
 
   const [errors, setErrors] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -32,7 +34,36 @@ const ContactForm = () => {
 
     if (Object.keys(newErrors).length === 0) {
       // Form is valid, submit the data
-      console.log("Form submitted", formData);
+      const templateParams = {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        phone: formData.phone,
+        email: formData.email,
+        companyName: formData.companyName,
+        message: formData.message,
+      };
+
+      emailjs
+        .send(
+          "service_r4f2t7c",
+          "template_fc0fypk",
+          templateParams,
+          "Ij2w3tj27AWy0pPSs"
+        )
+        .then((response) => {
+          console.log(response);
+          setFormData({
+            firstName: "",
+            lastName: "",
+            phone: "",
+            email: "",
+            companyName: "",
+            message: "",
+          });
+          setIsSubmitted(true);
+          //   alert("Thank you for your message!");
+        })
+        .catch((error) => console.error(error));
     }
   };
 
@@ -81,106 +112,112 @@ const ContactForm = () => {
         </div>
       </div>
       <div className="md:w-1/2 p-4">
-        <h2 className="text-2xl font-bold mb-4">Contact Inc Academy</h2>
+        <h2 className="text-2xl font-bold mb-4">Contact Babeltaelim.ae</h2>
         <p className="mb-4">Say Hello! We d love to chat.</p>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="flex flex-col md:flex-row md:space-x-4">
-            <div className="flex-1">
+        {isSubmitted ? (
+          <p className="text-green-500">
+            Your message has been sent successfully!
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="flex flex-col md:flex-row md:space-x-4">
+              <div className="flex-1">
+                <label className="block mb-1">
+                  First Name <span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  className={`w-full p-2 border ${
+                    errors.firstName ? "border-red-500" : "border-gray-300"
+                  } rounded`}
+                />
+                {errors.firstName && (
+                  <p className="text-red-500 text-sm">{errors.firstName}</p>
+                )}
+              </div>
+              <div className="flex-1">
+                <label className="block mb-1">
+                  Last Name <span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  className={`w-full p-2 border ${
+                    errors.lastName ? "border-red-500" : "border-gray-300"
+                  } rounded`}
+                />
+                {errors.lastName && (
+                  <p className="text-red-500 text-sm">{errors.lastName}</p>
+                )}
+              </div>
+            </div>
+            <div>
               <label className="block mb-1">
-                First Name <span className="text-red-500">*</span>
+                Phone <span className="text-red-500">*</span>
               </label>
               <input
                 type="text"
-                name="firstName"
-                value={formData.firstName}
+                name="phone"
+                value={formData.phone}
                 onChange={handleChange}
                 className={`w-full p-2 border ${
-                  errors.firstName ? "border-red-500" : "border-gray-300"
+                  errors.phone ? "border-red-500" : "border-gray-300"
                 } rounded`}
               />
-              {errors.firstName && (
-                <p className="text-red-500 text-sm">{errors.firstName}</p>
+              {errors.phone && (
+                <p className="text-red-500 text-sm">{errors.phone}</p>
               )}
             </div>
-            <div className="flex-1">
+            <div>
               <label className="block mb-1">
-                Last Name <span className="text-red-500">*</span>
+                Email <span className="text-red-500">*</span>
               </label>
               <input
-                type="text"
-                name="lastName"
-                value={formData.lastName}
+                type="email"
+                name="email"
+                value={formData.email}
                 onChange={handleChange}
                 className={`w-full p-2 border ${
-                  errors.lastName ? "border-red-500" : "border-gray-300"
+                  errors.email ? "border-red-500" : "border-gray-300"
                 } rounded`}
               />
-              {errors.lastName && (
-                <p className="text-red-500 text-sm">{errors.lastName}</p>
+              {errors.email && (
+                <p className="text-red-500 text-sm">{errors.email}</p>
               )}
             </div>
-          </div>
-          <div>
-            <label className="block mb-1">
-              Phone <span className="text-red-500">*</span>
-            </label>
-            <input
-              type="text"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-              className={`w-full p-2 border ${
-                errors.phone ? "border-red-500" : "border-gray-300"
-              } rounded`}
-            />
-            {errors.phone && (
-              <p className="text-red-500 text-sm">{errors.phone}</p>
-            )}
-          </div>
-          <div>
-            <label className="block mb-1">
-              Email <span className="text-red-500">*</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className={`w-full p-2 border ${
-                errors.email ? "border-red-500" : "border-gray-300"
-              } rounded`}
-            />
-            {errors.email && (
-              <p className="text-red-500 text-sm">{errors.email}</p>
-            )}
-          </div>
-          <div>
-            <label className="block mb-1">Company Name</label>
-            <input
-              type="text"
-              name="companyName"
-              value={formData.companyName}
-              onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div>
-            <label className="block mb-1">Message</label>
-            <textarea
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded"
-              rows="4"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded"
-          >
-            Submit
-          </button>
-        </form>
+            <div>
+              <label className="block mb-1">Company Name</label>
+              <input
+                type="text"
+                name="companyName"
+                value={formData.companyName}
+                onChange={handleChange}
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+            </div>
+            <div>
+              <label className="block mb-1">Message</label>
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                className="w-full p-2 border border-gray-300 rounded"
+                rows="4"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
